3932f1e32aa0b05092b814de44956a0db95ee645
KartikenBarnwal <kartikenkrb@gmail.com>
2024-12-14 17:59:49 +0530

    Initial Commit

---
+++ Infinite Page Controls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infinite Page Controls
+//
+//  Created by Kartiken Barnwal on 14/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
