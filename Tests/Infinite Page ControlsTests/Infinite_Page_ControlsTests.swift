174522ba41041221770ea9439678bc658119417d
KartikenBarnwal <kartikenkrb@gmail.com>
2024-12-17 21:53:41 +0530

    gif resource added for readme

---
+++ Tests/Infinite Page ControlsTests/Infinite_Page_ControlsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Infinite_Page_Controls
+
+final class Infinite_Page_ControlsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
