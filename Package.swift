6c73971a0c4544f9328aad0d7b8452af0597da0f
KartikenBarnwal <kartikenkrb@gmail.com>
2024-12-18 10:05:49 +0530

    updates swift package file

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Infinite Page Controls",
+    name: "InfinitePageControls",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Infinite Page Controls",
-            targets: ["Infinite Page Controls"]),
+            name: "InfinitePageControls",
+            targets: ["InfinitePageControls"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Infinite Page Controls"),
+            name: "InfinitePageControls"),
         .testTarget(
-            name: "Infinite Page ControlsTests",
-            dependencies: ["Infinite Page Controls"]),
+            name: "InfinitePageControlsTests",
+            dependencies: ["InfinitePageControls"]),
     ]
 )
