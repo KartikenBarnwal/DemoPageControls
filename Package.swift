be746553c476bfc18b017cc4020f9f4cff020d48
Kartiken Barnwal <64095433+KartikenBarnwal@users.noreply.github.com>
2024-12-17 23:43:58 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,28 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "InfinitePageControls",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "InfinitePageControls",
-            targets: ["InfinitePageControls"]),
+            targets: ["InfinitePageControls"]
+        ),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "InfinitePageControls"),
+            name: "InfinitePageControls",
+            dependencies: [],
+            path: "Sources/InfinitePageControls"
+        ),
         .testTarget(
             name: "InfinitePageControlsTests",
-            dependencies: ["InfinitePageControls"]),
+            dependencies: ["InfinitePageControls"],
+            path: "Tests/InfinitePageControlsTests"
+        ),
     ]
 )
